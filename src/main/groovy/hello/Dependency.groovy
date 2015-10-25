199b1e48e9a098865ad9926ccee4478ed0164d4a
luistrigueiros@gmail.com <luistrigueiros@gmail.com>
2015-10-25 20:20:34 +0000

    clean up

---
+++ src/main/groovy/hello/Dependency.groovy
@@ -0,0 +1,5 @@
+package hello
+
+class Dependency {
+    String message = 'Hello, dependency 1'
+}
--- src/main/groovy/hello/HelloWorld.groovy
+++
@@ -1,7 +0,0 @@
-package hello
-
-Date today = new Date()
-
-println "Hello World! ......, :-)"
-println "  today is " + today
-println " How do you do ?"
